31dc4434c1a234c4fe156025772ca6bff1593e9c
Nemesis502 <klawes-bastian@t-online.de>
2024-12-18 17:01:14 +0100

    now key up and down

--- scritps/game.js
+++ scritps/game.js
@@ -8,67 +8,47 @@ function init() {
 }
 
 window.addEventListener("keydown", (event) => {
-  switch (event.keyCode) {
-    case 87:
-    case 38:
-      up = true;
-      console.log("Up:", up);
-      break;
-
-    case 65:
-    case 37:
-      left = true;
-      console.log("Left:", left);
-      break;
-
-    case 83:
-    case 40:
-      down = true;
-      console.log("Down:", down);
-      break;
-
-    case 68:
-    case 39:
-      right = true;
-      console.log("Right:", right);
-      break;
-
-    case 32:
-      space = true;
-      console.log("Space:", space);
-      break;
+  if (event.keyCode === 87 || event.keyCode === 38) {
+    up = true;
+    console.log("Up:", up);
+  }
+  if (event.keyCode === 65 || event.keyCode === 37) {
+    left = true;
+    console.log("Left:", left);
+  }
+  if (event.keyCode === 83 || event.keyCode === 40) {
+    down = true;
+    console.log("Down:", down);
+  }
+  if (event.keyCode === 68 || event.keyCode === 39) {
+    right = true;
+    console.log("Right:", right);
+  }
+  if (event.keyCode === 32) {
+    space = true;
+    console.log("Space:", space);
   }
 });
 
 window.addEventListener("keyup", (event) => {
-  switch (event.keyCode) {
-    case 87:
-    case 38:
-      up = false;
-      console.log("Up:", up);
-      break;
-
-    case 65:
-    case 37:
-      left = false;
-      console.log("Left:", left);
-      break;
-
-    case 83:
-    case 40:
-      down = false;
-      console.log("Down:", down);
-      break;
-
-    case 68:
-    case 39:
-      right = false;
-      console.log("Right:", right);
-      break;
-
-    case 32:
-      space = false;
-      console.log("Space:", space);
-      break;
+  if (event.keyCode === 87 || event.keyCode === 38) {
+    up = false;
+    console.log("Up:", up);
+  }
+  if (event.keyCode === 65 || event.keyCode === 37) {
+    left = false;
+    console.log("Left:", left);
+  }
+  if (event.keyCode === 83 || event.keyCode === 40) {
+    down = false;
+    console.log("Down:", down);
+  }
+  if (event.keyCode === 68 || event.keyCode === 39) {
+    right = false;
+    console.log("Right:", right);
+  }
+  if (event.keyCode === 32) {
+    space = false;
+    console.log("Space:", space);
   }
 });
